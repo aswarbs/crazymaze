fb6532c6a86cb6ddfc0e1e029425d7bbce831b2c
Toby Clark <psytc9@nottingham.ac.uk>
2023-08-05 21:38:40 +0100

    add player spawn getters and setters to crazy_binary_maze.py

--- crazy_binary_maze.py
+++ crazy_binary_maze.py
@@ -13,5 +13,19 @@ class binary_maze():
         try: self.maze_height, height = kwargs.get("height")
         except: raise ValueError("binary_maze: maze intantiation, keyword 'height' not found")
 
+        # Creating a binary 2D array  of the map
         self.maze: list(int)
-        self.maze: [[True for y in range(0, height)] for x in range(0, width)]
+        self.maze: [[True for row in range(0, height)] for column in range(0, width)]
+
+        # Player Start Point Dictionary, default is None
+        self.player_spawn = dict(int, tuple(int, int)|None)
+        self.player_spawn[1], self.player_spawn[2] = None
+
+    # Getters & Setters for maze cells
+    def get_maze_cell(self, row: int, column: int) -> bool: return self.maze[row][column]
+    def set_maze_cell(self, row: int, column: int, value: bool) -> None: self.maze[row][column] = value
+
+    # Player Start Points Getters and Setters
+    def get_player_spawn(self, player_id: int) -> tuple(int, int)|None: return self.player_spawn[player_id]
+    def set_player_spawn(self, player_id: int, spawn_row: int, spawn_col: int) -> None: self.player_spawn[player_id] = (spawn_row, spawn_col)
+    def set_player_spawn(self, player_id: int, spawn: tuple(int, int)) -> None: self.player_spawn[player_id] = spawn
