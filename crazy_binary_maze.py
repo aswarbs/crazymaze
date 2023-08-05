b4a67b57516e48f42383ab5b5a38422a874890ee
Toby Clark <psytc9@nottingham.ac.uk>
2023-08-05 21:48:06 +0100

    add getters and setters for goal position to crazy_binary_maze.py

--- crazy_binary_maze.py
+++ crazy_binary_maze.py
@@ -21,6 +21,10 @@ class binary_maze():
         self.player_spawn = dict(int, tuple(int, int)|None)
         self.player_spawn[1], self.player_spawn[2] = None
 
+        # Goal Row and Column
+        self.goal_position: tuple(int, int)
+        self.goal_position = (None, None)
+
     # Getters & Setters for maze cells
     def get_maze_cell(self, row: int, column: int) -> bool: return self.maze[row][column]
     def set_maze_cell(self, row: int, column: int, value: bool) -> None: self.maze[row][column] = value
@@ -29,3 +33,7 @@ class binary_maze():
     def get_player_spawn(self, player_id: int) -> tuple(int, int)|None: return self.player_spawn[player_id]
     def set_player_spawn(self, player_id: int, spawn_row: int, spawn_col: int) -> None: self.player_spawn[player_id] = (spawn_row, spawn_col)
     def set_player_spawn(self, player_id: int, spawn: tuple(int, int)) -> None: self.player_spawn[player_id] = spawn
+
+    # Getters and setters for the goal position
+    def set_goal(self, goal_row: int, goal_column: int) -> None: self.goal_position = (goal_row, goal_column)
+    def set_goal(self, goal_position_tuple: tuple(int, int)): self.goal_position = goal_position_tuple
