2fcd22e92465997d723ab3f38d512d1ee619a0ac
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-08-08 13:25:07 +0100

    add comments to script runner

--- codebank/src_scriptrunner/crazy_script_runner.py
+++ codebank/src_scriptrunner/crazy_script_runner.py
@@ -4,19 +4,26 @@ import datetime
 import threading
 
 def run_script(script_path: str, rowcols: tuple[int, int], seed: int = None) -> Queue:
+    # Parameter overloading, calls base function
     return run_script(script_path, rowcols[0], rowcols[1], seed)
 
-def run_script(script_path: str, rows: int, columns: int, seed: int = None) -> Queue:
-    if seed is None: seed = 5
+def run_script(script_path: str, rows: int, columns: int, seed: int = 5) -> Queue:
 
+    # Gets a reference to the return queue (to run script asynchrnously)
     return_queue = Queue()
 
+    # Tries to open the script path
     file = open(script_path, 'r')
     script_content = file.read()
     file.close()
 
+    # Semi-anonymous function, as Thread needs functional argument
     def thread_function():
         exec(script_content, {'rows': rows, 'columns': columns, 'seed': seed, 'return_queue': return_queue})
     
+    # Runs thread
     thread = threading.Thread(target=thread_function)
     thread.start()
+
+    # Returns the return queue, when the script is complete, it will be added here
+    return return_queue
