95e01593ffa40b0b32a1f8a362b767cd3866437d
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-08-08 10:57:54 +0100

    add: logging to crazy_script_scanner.py

--- codebank/src_scriptrunner/crazy_script_scanner.py
+++ codebank/src_scriptrunner/crazy_script_scanner.py
@@ -1,15 +1,17 @@
 import os
+import logging
 
 def find_scripts(directory: str = "scriptbank/", show_errors: bool = False):
 
     # Get list of scripts using os module
     python_files: list(str)
     python_files = [os.path.join(root, file) for root, _, files in os.walk(directory) for file in files if file.endswith(".py")]
-    print(f"crazy_script_scanner: scanning {len(python_files)} files ending in .py in scriptbank/:\n ")
+    logging.debug(f"crazy_script_scanner: scanning {len(python_files)} files ending in .py in scriptbank/:\n ")
 
     # For each script, try and run it using a 10x10 test array, to ensure it is a valid file.
     for script_index, script_path in enumerate(python_files):
-        print(f"\tcrazy_script_scanner : script {script_index} : testing {script_path} with a 10x10 test maze")
+        logging.debug(f"\tcrazy_script_scanner : script {script_index} : testing {script_path} with a 10x10 test maze")
+
         try:
             # Open the file and read it
             with open(script_path, 'r') as file:
@@ -17,20 +19,20 @@ def find_scripts(directory: str = "scriptbank/", show_errors: bool = False):
             
             # Attempt to execute script
             exec(script_content)
-            print(f"\tcrazy_script_scanner : script {script_index} : successfully executed {script_index}\n")
+            logging.debug(f"\tcrazy_script_scanner : script {script_index} : successfully executed {script_index}\n")
 
         except Exception as script_failure_message:
             # On exception, flag as None and show error if applicable.
             python_files[script_index] = None
-            print(f"\tcrazy_script_scanner : script {script_index} : failed to run, {script_path} will no longer be selectable", end = ("\n\n" if not show_errors else "\n"))
+            logging.debug(f"\tcrazy_script_scanner : script {script_index} : failed to run, {script_path} will no longer be selectable" + ("\n" if not show_errors else ""))
 
-            if (show_errors): print(f"\t\t script {script_index} error: {script_failure_message}\n")
+            if (show_errors): logging.debug(f"\t\t script {script_index} error: {script_failure_message}\n")
 
         continue
 
     # Replace all flagged scripts (erroneous) & return
     python_files = list(filter(lambda value: value is not None, python_files))
-    print(f"crazy_script_scanner: scanning complete, {len(python_files)} scripts passed validation and will be accessible")
+    logging.debug(f"crazy_script_scanner: scanning complete, {len(python_files)} scripts passed validation and will be accessible\n")
     return python_files
 
 
--- crazymaze.py
+++ crazymaze.py
@@ -1,7 +1,11 @@
 from codebank.src_logic.crazy_controller import controller
 from codebank.src_scriptrunner.crazy_script_scanner import *
+import logging
+import datetime
 
 if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.DEBUG, filename=f"logbank/crazymaze-{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log", filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 
     x = find_scripts("scriptbank/", True)
 
