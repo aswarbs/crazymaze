f90d17af890ab78d56686c1aab283731a9bc0ae3
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-08-08 11:11:54 +0100

    fix gitignore to work recursively in subfolders

--- crazymaze.py
+++ crazymaze.py
@@ -3,9 +3,6 @@ from codebank.src_scriptrunner.crazy_script_scanner import *
 from codebank.logger import *
 
 if __name__ == "__main__":
-
-    
     x = find_scripts("scriptbank/", True)
-
     controller = controller()
     exit()
