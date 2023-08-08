29dc8d49ec5ee8db133946a5062c02c89787f81a
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-08-08 13:23:05 +0100

    add working script runner to repository

--- codebank/src_scriptrunner/crazy_script_runner.py
+++ codebank/src_scriptrunner/crazy_script_runner.py
@@ -1,9 +1,22 @@
 from codebank.logger import *
 from queue import Queue
 import datetime
+import threading
 
 def run_script(script_path: str, rowcols: tuple[int, int], seed: int = None) -> Queue:
     return run_script(script_path, rowcols[0], rowcols[1], seed)
 
 def run_script(script_path: str, rows: int, columns: int, seed: int = None) -> Queue:
-    if seed is None: seed = int(datetime.now())
+    if seed is None: seed = 5
+
+    return_queue = Queue()
+
+    file = open(script_path, 'r')
+    script_content = file.read()
+    file.close()
+
+    def thread_function():
+        exec(script_content, {'rows': rows, 'columns': columns, 'seed': seed, 'return_queue': return_queue})
+    
+    thread = threading.Thread(target=thread_function)
+    thread.start()
--- crazymaze.py
+++ crazymaze.py
@@ -1,8 +1,12 @@
 from codebank.src_logic.crazy_controller import controller
 from codebank.src_scriptrunner.crazy_script_scanner import *
+from codebank.src_scriptrunner.crazy_script_runner import *
 from codebank.logger import *
 
 if __name__ == "__main__":
     x = find_scripts("scriptbank/", True)
+
+    run_script("scriptbank/test.py", 5, 5)
+
     controller = controller()
     exit()
