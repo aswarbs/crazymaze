9d259b54db656b992b132057a54ed6544b7ed611
tobybenjaminclark <tobybenjaminclark@gmail.com>
2023-08-10 23:18:23 +0100

    add comments and documentation to convertor and runner

--- codebank/src_scriptrunner/crazy_script_runner.py
+++ codebank/src_scriptrunner/crazy_script_runner.py
@@ -4,10 +4,18 @@ import datetime
 import threading
 
 def run_script(script_path: str, rowcols: tuple[int, int], seed: int = None) -> Queue:
+    """
+    Runs the passed script, proxy function to run_script with non-tuple rows and columns.
+    """
+
     # Parameter overloading, calls base function
     return run_script(script_path, rowcols[0], rowcols[1], seed)
 
 def run_script(script_path: str, rows: int, columns: int, seed: int = 5) -> Queue:
+    """
+    Runs the passed script on a thread, returns a queue that the resulting data will be
+    added to.
+    """
 
     # Gets a reference to the return queue (to run script asynchrnously)
     return_queue = Queue()
